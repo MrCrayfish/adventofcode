372f5b3486984192896d4966815c0771a8a83305
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-12-12 02:45:14 +1030

    📦 Further D11:P2 optimisations

--- day11/part2.cpp
+++ day11/part2.cpp
@@ -1,11 +1,6 @@
-#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <map>
-#include <memory>
-#include <numeric>
 #include <set>
-#include <sstream>
 #include <string>
 #include <vector>
 
@@ -13,24 +8,10 @@ struct Galaxy {
     int x, y;
 };
 
-class Path {
-public:
-    Galaxy *a;
-    Galaxy *b;
-    int distance;
-
-    Path(Galaxy &a, Galaxy &b) {
-        this->a = &a;
-        this->b = &b;
-        this->distance = std::abs(a.x - b.x) + std::abs(a.y - b.y);
-    }
-};
-
 main() {
     std::vector<Galaxy> galaxies;
-    std::vector<Path> paths;
-    std::set<int> occupied_columns;
-    std::set<int> occupied_rows;
+    std::set<int> galaxy_columns;
+    std::set<int> galaxy_rows;
 
     int y = 0;
     std::string line;
@@ -39,39 +20,41 @@ main() {
         int x = 0;
         for (char &c : line) {
             if (c == '#') {
-                Galaxy galaxy{x, y};
-                galaxies.push_back(galaxy);
-                occupied_columns.insert(x);
-                occupied_rows.insert(y);
+                galaxies.push_back({x, y});
+                galaxy_columns.insert(x);
+                galaxy_rows.insert(y);
             }
             x++;
         }
         y++;
     }
 
+    int empty_space_size = 1000000 - 1;
+    size_t sum = 0;
     for (int i = 0; i < galaxies.size(); i++) {
         for (int j = i + 1; j < galaxies.size(); j++) {
-            Path path(galaxies[i], galaxies[j]);
-            paths.push_back(path);
-        }
-    }
+            Galaxy *a = &galaxies[i];
+            Galaxy *b = &galaxies[j];
 
-    int empty_space_size = 1000000;
-    size_t sum = 0;
-    for (auto &p : paths) {
-        sum += p.distance;
-        int min_x = std::min<int>(p.a->x, p.b->x);
-        int max_x = std::max<int>(p.a->x, p.b->x);
-        for (int x = min_x + 1; x < max_x; x++) {
-            if (occupied_columns.find(x) == occupied_columns.end()) {
-                sum += empty_space_size - 1;
+            // Add the manhatten distance
+            sum += std::abs(a->x - b->x) + std::abs(a->y - b->y);
+
+            // Find any empty columns and add the aditional distance
+            int min_x = std::min<int>(a->x, b->x);
+            int max_x = std::max<int>(a->x, b->x);
+            for (int x = min_x + 1; x < max_x; x++) {
+                if (galaxy_columns.find(x) == galaxy_columns.end()) {
+                    sum += empty_space_size;
+                }
             }
-        }
-        int min_y = std::min<int>(p.a->y, p.b->y);
-        int max_y = std::max<int>(p.a->y, p.b->y);
-        for (int y = min_y + 1; y < max_y; y++) {
-            if (occupied_rows.find(y) == occupied_rows.end()) {
-                sum += empty_space_size - 1;
+
+            // Find any empty rows and add the aditional distance
+            int min_y = std::min<int>(a->y, b->y);
+            int max_y = std::max<int>(a->y, b->y);
+            for (int y = min_y + 1; y < max_y; y++) {
+                if (galaxy_rows.find(y) == galaxy_rows.end()) {
+                    sum += empty_space_size;
+                }
             }
         }
     }
